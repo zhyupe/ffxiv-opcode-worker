383e4770d42f3e95f733f23171cbf46c02f41d96
Innpaul Zhang <zyp108421@gmail.com>
2021-08-12 22:36:47 +0800

    fix: update constant file

--- worker/machina-wrapper-json/index.js
+++ worker/machina-wrapper-json/index.js
@@ -60,8 +60,10 @@ using System.Collections.Generic;
 
 namespace FFXIVConstants
 {
-${tab}static class CN
+${tab}public static class CN
 ${tab}{
+${tab}${tab}public const string Version = "${version}";
+
 ${tab}${tab}public static Dictionary<string, object> Constants = new Dictionary<string, object>
 ${tab}${tab}{
 ${tab}${tab}${tab}{ "InventoryOperationBaseValue", 0x01B8 },
